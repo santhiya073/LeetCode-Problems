bb01013eac3e29c94f28315574371ab392a6775e
santhiya073 <santhiya732008@gmail.com>
2026-07-20 20:17:04 +0530

    Create Max-numofWordsFound2114.java

---
+++ Max-numofWordsFound2114.java
@@ -0,0 +1,13 @@
+class  mostWordsFound {
+    public int mostWordsFound(String[] sentences) {
+        
+      int max=0;
+
+      for(String s:sentences){
+        int words = s.split(" ").length;
+        max = Math.max(max, words);
+      }
+
+      return max;
+    }
+}
