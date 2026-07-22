8470c02b3b7c196c3bf0826fa2571ba17aa1300f
santhiya073 <santhiya732008@gmail.com>
2026-07-22 21:42:06 +0530

    Create Jewels-and-stones771.java

---
+++ Jewels-and-stones771.java
@@ -0,0 +1,13 @@
+class Jewelsandstones {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+
+        for (int i = 0; i < stones.length(); i++) {
+            if (jewels.indexOf(stones.charAt(i)) != -1) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
